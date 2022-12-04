809c2d3b994fb4ecafbf072f96364c28d1dd0f30
Jack Meng <jackmeng0814@gmail.com>
2022-12-04 17:53:59 -0500

    `make`

--- pkg/src/com/jackmeng/halcyon/gui_HalcyonFrame.java
+++ pkg/src/com/jackmeng/halcyon/gui_HalcyonFrame.java
@@ -29,8 +29,7 @@ public class gui_HalcyonFrame
       private static final Dimension MINIMUM_SIZE = new Dimension(10, 10);
       private static final Dimension MAXIMUM_SIZE = new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE);
 
-      private static final Map< Integer, Integer > cursors = new HashMap<>();
-      static
+      private static Map< Integer, Integer > cursors = new HashMap<>();
       {
         cursors.put(1, Cursor.N_RESIZE_CURSOR);
         cursors.put(2, Cursor.W_RESIZE_CURSOR);
@@ -60,11 +59,56 @@ public class gui_HalcyonFrame
       private Dimension minimumSize = MINIMUM_SIZE;
       private Dimension maximumSize = MAXIMUM_SIZE;
 
+      /**
+       * Convenience contructor. All borders are resizable in increments of
+       * a single pixel. Components must be registered separately.
+       */
       public ComponentResizer()
       {
         this(new Insets(5, 5, 5, 5), new Dimension(1, 1));
       }
 
+      /**
+       * Convenience contructor. All borders are resizable in increments of
+       * a single pixel. Components can be registered when the class is created
+       * or they can be registered separately afterwards.
+       *
+       * @param components
+       *          components to be automatically registered
+       */
+      public ComponentResizer(Component... components)
+      {
+        this(new Insets(5, 5, 5, 5), new Dimension(1, 1), components);
+      }
+
+      /**
+       * Convenience contructor. Eligible borders are resisable in increments of
+       * a single pixel. Components can be registered when the class is created
+       * or they can be registered separately afterwards.
+       *
+       * @param dragInsets
+       *          Insets specifying which borders are eligible to be
+       *          resized.
+       * @param components
+       *          components to be automatically registered
+       */
+      public ComponentResizer(Insets dragInsets, Component... components)
+      {
+        this(dragInsets, new Dimension(1, 1), components);
+      }
+
+      /**
+       * Create a ComponentResizer.
+       *
+       * @param dragInsets
+       *          Insets specifying which borders are eligible to be
+       *          resized.
+       * @param snapSize
+       *          Specify the dimension to which the border will snap to
+       *          when being dragged. Snapping occurs at the halfway mark.
+       * @param components
+       *          components to be automatically registered
+       */
       public ComponentResizer(Insets dragInsets, Dimension snapSize, Component... components)
       {
         setDragInsets(dragInsets);
@@ -72,6 +116,26 @@ public class gui_HalcyonFrame
         registerComponent(components);
       }
 
+      /**
+       * Get the drag insets
+       *
+       * @return the drag insets
+       */
+      public Insets getDragInsets()
+      {
+        return dragInsets;
+      }
+
+      /**
+       * Set the drag dragInsets. The insets specify an area where mouseDragged
+       * events are recognized from the edge of the border inwards. A value of
+       * 0 for any size will imply that the border is not resizable. Otherwise
+       * the appropriate drag cursor will appear when the mouse is inside the
+       * resizable border area.
+       *
+       * @param dragInsets
+       *          Insets to control which borders are resizeable.
+       */
       public void setDragInsets(Insets dragInsets)
       {
         validateMinimumAndInsets(minimumSize, dragInsets);
@@ -79,11 +143,45 @@ public class gui_HalcyonFrame
         this.dragInsets = dragInsets;
       }
 
+      /**
+       * Get the components maximum size.
+       *
+       * @return the maximum size
+       */
+      public Dimension getMaximumSize()
+      {
+        return maximumSize;
+      }
+
+      /**
+       * Specify the maximum size for the component. The component will still
+       * be constrained by the size of its parent.
+       *
+       * @param maximumSize
+       *          the maximum size for a component.
+       */
       public void setMaximumSize(Dimension maximumSize)
       {
         this.maximumSize = maximumSize;
       }
 
+      /**
+       * Get the components minimum size.
+       *
+       * @return the minimum size
+       */
+      public Dimension getMinimumSize()
+      {
+        return minimumSize;
+      }
+
+      /**
+       * Specify the minimum size for the component. The minimum size is
+       * constrained by the drag insets.
+       *
+       * @param minimumSize
+       *          the minimum size for a component.
+       */
       public void setMinimumSize(Dimension minimumSize)
       {
         validateMinimumAndInsets(minimumSize, dragInsets);
@@ -91,6 +189,12 @@ public class gui_HalcyonFrame
         this.minimumSize = minimumSize;
       }
 
+      /**
+       * Remove listeners from the specified component
+       *
+       * @param component
+       *          the component the listeners are removed from
+       */
       public void deregisterComponent(Component... components)
       {
         for (Component component : components)
@@ -100,6 +204,12 @@ public class gui_HalcyonFrame
         }
       }
 
+      /**
+       * Add the required listeners to the specified component
+       *
+       * @param component
+       *          the component the listeners are added to
+       */
       public void registerComponent(Component... components)
       {
         for (Component component : components)
@@ -109,11 +219,35 @@ public class gui_HalcyonFrame
         }
       }
 
+      /**
+       * Get the snap size.
+       *
+       * @return the snap size.
+       */
+      public Dimension getSnapSize()
+      {
+        return snapSize;
+      }
+
+      /**
+       * Control how many pixels a border must be dragged before the size of
+       * the component is changed. The border will snap to the size once
+       * dragging has passed the halfway mark.
+       *
+       * @param snapSize
+       *          Dimension object allows you to separately spcify a
+       *          horizontal and vertical snap size.
+       */
       public void setSnapSize(Dimension snapSize)
       {
         this.snapSize = snapSize;
       }
 
+      /**
+       * When the components minimum size is less than the drag insets then
+       * we can't determine which border should be resized so we need to
+       * prevent this from happening.
+       */
       private void validateMinimumAndInsets(Dimension minimum, Insets drag)
       {
         int minimumWidth = drag.left + drag.right;
@@ -127,6 +261,8 @@ public class gui_HalcyonFrame
         }
       }
 
+      /**
+       */
       @Override
       public void mouseMoved(MouseEvent e)
       {
@@ -146,9 +282,13 @@ public class gui_HalcyonFrame
         if (location.y > source.getHeight() - dragInsets.bottom - 1)
           direction += SOUTH;
 
+        // Mouse is no longer over a resizable border
+
         if (direction == 0)
+        {
           source.setCursor(sourceCursor);
-        else
+        }
+        else // use the appropriate resizable cursor
         {
           int cursorType = cursors.get(direction);
           Cursor cursor = Cursor.getPredefinedCursor(cursorType);
@@ -179,13 +319,24 @@ public class gui_HalcyonFrame
       @Override
       public void mousePressed(MouseEvent e)
       {
+        // The mouseMoved event continually updates this variable
+
         if (direction == 0)
           return;
+
+        // Setup for resizing. All future dragging calculations are done based
+        // on the original bounds of the component and mouse pressed location.
+
         resizing = true;
+
         Component source = e.getComponent();
         pressed = e.getPoint();
         SwingUtilities.convertPointToScreen(pressed, source);
         bounds = source.getBounds();
+
+        // Making sure autoscrolls is false will allow for smoother resizing
+        // of components
+
         if (source instanceof JComponent)
         {
           JComponent jc = (JComponent) source;
@@ -194,6 +345,9 @@ public class gui_HalcyonFrame
         }
       }
 
+      /**
+       * Restore the original state of the Component
+       */
       @Override
       public void mouseReleased(MouseEvent e)
       {
@@ -203,13 +357,23 @@ public class gui_HalcyonFrame
         source.setCursor(sourceCursor);
 
         if (source instanceof JComponent)
+        {
           ((JComponent) source).setAutoscrolls(autoscrolls);
+        }
       }
 
+      /**
+       * Resize the component ensuring location and size is within the bounds
+       * of the parent container and that the size is within the minimum and
+       * maximum constraints.
+       *
+       * All calculations are done using the bounds of the component when the
+       * resizing started.
+       */
       @Override
       public void mouseDragged(MouseEvent e)
       {
-        if (!resizing)
+        if (resizing == false)
           return;
 
         Component source = e.getComponent();
@@ -221,10 +385,15 @@ public class gui_HalcyonFrame
 
       protected void changeBounds(Component source, int direction, Rectangle bounds, Point pressed, Point current)
       {
+        // Start with original locaton and size
+
         int x = bounds.x;
         int y = bounds.y;
         int width = bounds.width;
         int height = bounds.height;
+
+        // Resizing the West or North border affects the size and location
+
         if (WEST == (direction & WEST))
         {
           int drag = getDragDistance(pressed.x, current.x, snapSize.width);
@@ -244,6 +413,9 @@ public class gui_HalcyonFrame
           y -= drag;
           height += drag;
         }
+
+        // Resizing the East or South border only affects the size
+
         if (EAST == (direction & EAST))
         {
           int drag = getDragDistance(current.x, pressed.x, snapSize.width);
@@ -266,6 +438,9 @@ public class gui_HalcyonFrame
         source.validate();
       }
 
+      /*
+       * Determine how far the mouse has moved from where dragging started
+       */
       private int getDragDistance(int larger, int smaller, int snapSize)
       {
         int halfway = snapSize / 2;
@@ -276,6 +451,9 @@ public class gui_HalcyonFrame
         return drag;
       }
 
+      /*
+       * Adjust the drag value to be within the minimum and maximum range.
+       */
       private int getDragBounded(int drag, int snapSize, int dimension, int minimum, int maximum)
       {
         while (dimension + drag < minimum)
@@ -287,6 +465,9 @@ public class gui_HalcyonFrame
         return drag;
       }
 
+      /*
+       * Keep the size of the component within the bounds of its parent.
+       */
       private Dimension getBoundingSize(Component source)
       {
         if (source instanceof Window)
@@ -296,7 +477,9 @@ public class gui_HalcyonFrame
           return new Dimension(bounds.width, bounds.height);
         }
         else
+        {
           return source.getParent().getSize();
+        }
       }
     }
 
@@ -362,6 +545,7 @@ public class gui_HalcyonFrame
             || (content.getMaximumSize().width <= frame.getMaximumSize().width
                 && content.getMaximumSize().height <= frame.getMaximumSize().height))
         {
+          frame.getRootPane().setWindowDecorationStyle(JRootPane.NONE);
           cr.registerComponent(frame);
           frame.addMouseListener(cr);
           frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
@@ -413,8 +597,9 @@ public class gui_HalcyonFrame
         titleBarStr.setVerticalAlignment(SwingConstants.CENTER);
 
         JPanel btns = new JPanel();
-        btns.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 0));
+        btns.setLayout(new BoxLayout(btns, BoxLayout.X_AXIS));
         btns.setOpaque(true);
+        btns.setBorder(BorderFactory.createLineBorder(Color.PINK));
         btns.setAlignmentY(Component.CENTER_ALIGNMENT);
         btns.setBackground(conf.bg);
 
@@ -430,6 +615,7 @@ public class gui_HalcyonFrame
           }));
         }
         btns.add(gen_Button(conf.bgClose, frame::dispose));
+
         JLabel titleBarICO = new JLabel(conf.icon != null
             ? new ImageIcon(conf.icon.getImage().getScaledInstance(contentOff, contentOff, Image.SCALE_AREA_AVERAGING))
             : new ImageIcon());
@@ -460,8 +646,9 @@ public class gui_HalcyonFrame
           @Override
           public void componentResized(ComponentEvent e)
           {
-              titleBar.setSize(new Dimension(frame.getSize().width, TITLEBAR_HEIGHT_REFERENDUM));
-              content.setPreferredSize(new Dimension(frame.getSize().width, frame.getSize().height - TITLEBAR_HEIGHT_REFERENDUM));
+            titleBar.setSize(new Dimension(frame.getSize().width, TITLEBAR_HEIGHT_REFERENDUM));
+            content.setPreferredSize(
+                new Dimension(frame.getSize().width, frame.getSize().height - TITLEBAR_HEIGHT_REFERENDUM));
           }
         });
         titleBar.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -474,7 +661,8 @@ public class gui_HalcyonFrame
         frame.getContentPane().add(Box.createHorizontalGlue());
         cr.setMinimumSize(frame.getMinimumSize());
 
-        // all the above stuffs make the titlebar be pushed upwards so it doesn't go wonky on a custom titlebar
+        // all the above stuffs make the titlebar be pushed upwards so it doesn't go
+        // wonky on a custom titlebar
         // this took a lot of debugging and mindless code breaking to get it to work!!!
         // also pls fix code, bc rn this code is kind of lets say messy :|
 
@@ -580,8 +768,8 @@ public class gui_HalcyonFrame
         setContentAreaFilled(false);
         setAlignmentX(Component.CENTER_ALIGNMENT);
         setAlignmentY(Component.CENTER_ALIGNMENT);
-        setPreferredSize(new Dimension(size + 1, size + 1));
-        setMaximumSize(new Dimension(size + 1, size + 1));
+        setPreferredSize(new Dimension(size, size));
+        setMaximumSize(new Dimension(size, size));
       }
 
       @Override
@@ -680,10 +868,6 @@ public class gui_HalcyonFrame
         System.exit(0);
       }
     });
-
-    frame.expose().getRootPane().setBorder(
-        BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(const_ColorManager.DEFAULT_GREEN_FG, 1),
-            BorderFactory.createLineBorder(const_ColorManager.DEFAULT_BG, 5)));
 
     /*---------------------------------------------------------------------------------------------------------------- /
     / frame.expose().setShape(new java.awt.geom.RoundRectangle2D.Double(0, 0, frame.expose().getPreferredSize().width, /
