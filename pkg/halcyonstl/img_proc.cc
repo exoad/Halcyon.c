5a9750087665ff6084461c40915b502028d11d21
Jack Meng <jackmeng0814@gmail.com>
2022-12-14 20:14:43 -0500

    `add`

--- pkg/halcyonstl/img_proc.cc
+++ pkg/halcyonstl/img_proc.cc
@@ -1,3 +1,7 @@
+#include <algorithm>
+#include <cmath>
+
+#include "cstring"
 #include "include/com_jackmeng_util_sys_sys_ImageProcessor.h"
 
 JNIEXPORT jint JNICALL
@@ -52,4 +56,57 @@ Java_com_jackmeng_util_sys_sys_1ImageProcessor_img_1accent_1color_1palette(
   env->ReleaseIntArrayElements(pixels, pixelsElements, 0);
   env->ReleaseIntArrayElements(palette, paletteElements, 0);
   return palette;
+}
+
+void muc_fill(int *px, int tolerance, int desired, int target, int width,
+              int height) {
+  for (int i = 0; i < (width * height); i++) {
+    int r = (px[i] & 0xff0000) >> 16;
+    int g = (px[i] & 0x00ff00) >> 8;
+    int b = px[i] & 0x0000ff;
+    if (abs(r - target) <= tolerance && abs(g - target) <= tolerance &&
+        abs(b - target) <= tolerance) {
+      px[i] = desired;
+    }
+  }
+}
+
+void sharpen_tone(int *px, int strength, int width, int height) {
+  int *cpy = new int[width * height];
+  std::memcpy(cpy, px, width * height * sizeof(int));
+  for (int i = 0; i < width * height; i++) {
+    int x = i % width;
+    int y = i / width;
+    int s = 0;
+    s += -1 * cpy[(x - 1) + (y - 1) * width];
+    s += -1 * cpy[(x - 1) + (y + 0) * width];
+    s += -1 * cpy[(x - 1) + (y + 1) * width];
+    s += -1 * cpy[(x + 0) + (y - 1) * width];
+    s += 9 * cpy[(x + 0) + (y + 0) * width];
+    s += -1 * cpy[(x + 0) + (y + 1) * width];
+    s += -1 * cpy[(x + 1) + (y - 1) * width];
+    s += -1 * cpy[(x + 1) + (y + 0) * width];
+    s += -1 * cpy[(x + 1) + (y + 1) * width];
+    px[i] = std::max(0, std::min(s * strength, 255));
+  }
+  delete[] cpy;
+}
+
+void tint(int *px, int tint, int strength, int width, int height) {
+  for (int i = 0; i < width * height; i++) {
+    int r = (px[i] & 0xff0000) >> 16;
+    int g = (px[i] & 0x00ff00) >> 8;
+    int b = px[i] & 0x0000ff;
+    int tr = (tint & 0xff0000) >> 16;
+    int tg = (tint & 0x00ff00) >> 8;
+    int tb = tint & 0x0000ff;
+    r = (r * (255 - strength) + tr * strength) / 255;
+    g = (g * (255 - strength) + tg * strength) / 255;
+    b = (b * (255 - strength) + tb * strength) / 255;
+    px[i] = (r << 16) | (g << 8) | b;
+  }
+}
+
+void gaussian_blur_1(int*pixels, int strength, int width, int height) {
+
 }
