9b5282aea897dbefacfa8d59243e2d0c0042e31b
Jack Meng <jackmeng0814@gmail.com>
2022-12-30 08:36:39 -0500

    `add`

---
+++ pkg/src/com/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * All Jack Meng integrated Java libraries and applications.
+ *
+ * For this particular program, the "Halcyon" program is implemented.
+ *
+ * Copyright (C) Jack Meng 2021-2023
+ *
+ * @author Jack Meng
+ */
+
+package com;
